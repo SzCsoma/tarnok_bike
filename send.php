1cfe28e51447e1d8249cf0252d61a613f06ddcd0
Széles Csoma <szeles@inf.u-szeged.hu>
2026-03-06 15:45:55 +0100

    good to go

---
+++ send.php
@@ -0,0 +1,42 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nev = htmlspecialchars($_POST['nev']);
+    $email = htmlspecialchars($_POST['email']);
+    $uzenet = htmlspecialchars($_POST['uzenet']);
+
+    $to = "kerekparbolt.tarnok@gmail.com";
+
+    $subject = "Új üzenet a weboldalról";
+
+    $message = "
+    Új üzenet érkezett a weboldalról:
+
+    Név: $nev
+    Email: $email
+
+    Üzenet:
+    $uzenet
+    ";
+
+    $headers = "From: $email";
+
+    if(mail($to, $subject, $message, $headers)){
+
+        echo "<script>
+        alert('Az üzenet elküldve!');
+        window.location.href='kapcsolat.html';
+        </script>";
+
+    } else {
+
+        echo "<script>
+        alert('Hiba történt az üzenet küldésekor.');
+        window.location.href='kapcsolat.html';
+        </script>";
+
+    }
+
+}
+?>
